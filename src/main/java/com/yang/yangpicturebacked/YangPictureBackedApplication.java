33dc5e16e58dbd51007e4f8d51c18b6f2c1723b1
yangli <yangpili1997@gmail.com>
2026-02-14 13:44:02 +0800

    项目初始化

---
+++ src/main/java/com/yang/yangpicturebacked/YangPictureBackedApplication.java
@@ -0,0 +1,13 @@
+package com.yang.yangpicturebacked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YangPictureBackedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YangPictureBackedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yang/yangpicturebacked/YangPictureBackedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yang.yangpicturebacked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YangPictureBackedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
