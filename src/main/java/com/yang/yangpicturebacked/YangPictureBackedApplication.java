9548125ceede18c13c093dc1a7611740654fc91b
yangli <yangpili1997@gmail.com>
2026-02-27 12:15:35 +0800

    用户模块

--- src/main/java/com/yang/yangpicturebacked/YangPictureBackedApplication.java
+++ src/main/java/com/yang/yangpicturebacked/YangPictureBackedApplication.java
@@ -1,9 +1,14 @@
 package com.yang.yangpicturebacked;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@MapperScan("com.yang.yangpicturebacked.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class YangPictureBackedApplication {
 
     public static void main(String[] args) {
