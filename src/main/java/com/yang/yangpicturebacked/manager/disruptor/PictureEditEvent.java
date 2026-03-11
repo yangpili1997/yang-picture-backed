c185d6a06fe4076ed97b76ee28974677148ae652
yangli <yangpili1997@gmail.com>
2026-03-11 13:19:14 +0800

    图片协同编辑

---
+++ src/main/java/com/yang/yangpicturebacked/manager/disruptor/PictureEditEvent.java
@@ -0,0 +1,31 @@
+package com.yang.yangpicturebacked.manager.disruptor;
+
+import com.yang.yangpicturebacked.manager.websocket.model.PictureEditRequestMessage;
+import com.yang.yangpicturebacked.model.entity.User;
+import lombok.Data;
+import org.springframework.web.socket.WebSocketSession;
+
+@Data
+public class PictureEditEvent {
+
+    /**
+     * 消息
+     */
+    private PictureEditRequestMessage pictureEditRequestMessage;
+
+    /**
+     * 当前用户的 session
+     */
+    private WebSocketSession session;
+    
+    /**
+     * 当前用户
+     */
+    private User user;
+
+    /**
+     * 图片 id
+     */
+    private Long pictureId;
+
+}
